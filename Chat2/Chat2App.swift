0194689cc797d8bc4cb2bff0b825c30e1a301854
1anotherlake <tkdgh811@gmail.com>
2023-05-01 22:39:54 +0900

    Initial Commit

---
+++ Chat2/Chat2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chat2App.swift
+//  Chat2
+//
+//  Created by 김상호 on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Chat2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
